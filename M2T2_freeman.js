308a0a00683b72dc12bd91b3c0210001b1abe0ec
freemanm9014 <35772540+freemanm9014@users.noreply.github.com>
2018-02-20 11:53:58 -0500

    Add files via upload

---
+++ M2T2_freeman.js
@@ -0,0 +1,38 @@
+﻿// WEB 151
+// M2T2_freeman
+// 02/20
+
+
+console.log('spinach');
+console.log('superman');
+
+
+console.log('JavaScript');
+console.log(33.7);
+console.log(true);
+console.log(null);
+
+
+console.log(3.5 + 22); // Equals 25.5
+console.log(2018 - 1969); // Equals 49
+console.log(65 /240 ); // Equals 0.27
+console.log(0.2708 * 100); // Equals 
+
+
+console.log('Teaching the world how to code'.length);
+
+
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase()); 
+
+
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim()); 
+
+
+const entree = 'Enchiladas';
+console.log(entree);
+// Output: Enchiladas
+const price = 12;
+console.log(price);
+entree = 'Tacos';
